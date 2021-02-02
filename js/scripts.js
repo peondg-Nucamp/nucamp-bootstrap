c09135c965a1a46a223ed4797205dc534d0de86d
daniel-peon <dgpeon62@gmail.com>
2021-02-02 12:13:21 -0500

    Edited script

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 $(function() {
     $(".carousel").carousel( { interval: 2000, pause: false } );
     $("#carouselButton").on("click", function() {
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause");
-        }
-    });
+            if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+                $(".carousel").carousel("pause");
+                $("#carouselButton").children("i").removeClass("fa-pause");
+                $("#carouselButton").children("i").addClass("fa-play");
+            } else {
+                $(".carousel").carousel("cycle");
+                $("#carouselButton").children("i").removeClass("fa-play");
+                $("#carouselButton").children("i").addClass("fa-pause");
+            }
+        });
 });
