3eb50715a216f01993fc45f35b19e62f89f7780e
daniel-peon <dgpeon62@gmail.com>
2021-02-01 22:33:22 -0500

    Updated click event handler in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(function() {
     $(".carousel").carousel( { interval: 2000, pause: false } );
-    $("#carouselButton").on(function(){
-        if ($("#carouselButton").children("i").hasClass("fa-pause") ) {
+    $("#carouselButton").on("click", () => {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
             $("#carouselButton").children("i").addClass("fa-play");
         } else {
-            $(".carousel").carousel("cycle"); 
+            $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause");
         }
