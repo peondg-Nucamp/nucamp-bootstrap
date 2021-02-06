76669877f40e7ca1846bf11bcb9ad647a24d2566
daniel-peon <dgpeon62@gmail.com>
2021-02-06 12:47:01 -0500

    Week 4 Assignment Updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 $(function() {
     $(".carousel").carousel( { interval: 2000, pause: false } );
     $("#carouselButton").click(function() {
-    // $("#carouselButton").on("click", function() {
             if ($("#carouselButton").children("i").hasClass("fa-pause")) {
                 $(".carousel").carousel("pause");
                 $("#carouselButton").children("i").removeClass("fa-pause");
